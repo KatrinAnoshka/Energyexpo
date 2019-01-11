b8d66b2a81ff578293a91d7f1cdebc103f13c6a1
KatrinAnoshka <nosha309@mail.ru>
2019-01-11 16:04:48 +0300

    before

--- js/canvas.js
+++ js/canvas.js
@@ -305,7 +305,9 @@ $(document).ready(function() {
         return array[Math.floor(Math.random()*array.length)];
     };
 
-pna = new ParticleNetworkAnimation();   pna.init($('.particle-network-animation')[0]);
+pna = new ParticleNetworkAnimation();   
+
+pna.init($('.particle-network-animation')[0]);
 
 }());
 
